07c35190581b99788070f22ac12573350a9cd278
Aybüke Ceren Duran <36036370+cerendrnn@users.noreply.github.com>
2019-03-22 13:44:35 +0300

    Add files via upload

---
+++ question4_part4.m
@@ -0,0 +1,15 @@
+%CS464 Introduction to Machine Learning Homework-01
+%Author: Aybüke Ceren Duran
+%Section-01
+%21302686
+%Date: 24.03.2019
+
+format compact
+
+train_features  = importdata('question-4-train-features.csv');
+train_labels = importdata('question-4-train-labels.csv');
+disp(train_labels);
+test_features = importdata('question-4-test-features.csv');
+test_labels = importdata('question-4-test-labels.csv');
+disp(test_labels);
+
